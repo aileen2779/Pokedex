dc64a68f298c7adae6399339d40c9afa9050fbc5
Gamy Malasarte <gmaasarte@Gamys-Mac.local>
2017-06-09 17:59:06 -0700

    modified login with DB authentication

--- pokedex3/MainController.swift
+++ pokedex3/MainController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class MainController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var loginTextField: CustomTextField!
     
+    
+    let url  = "http://www.702shifters.com/ios_auth.php"
+    
+    //@IBOutlet weak var username_input: UITextField!
+    //@IBOutlet weak var password_input: UITextField!
+    //@IBOutlet weak var login_button: UIButton!
+    
+    @IBOutlet weak var login_button: UIButton!
+    
+    var login_session:String = ""
+    
+    @IBOutlet weak var loginTextField: CustomTextField!
     @IBOutlet weak var passwordTextField: CustomTextField!
 
     var searchURL = URL(string: "http://www.702shifters.com?user=gamy&pass=gamy666")
@@ -22,11 +33,6 @@ class MainController: UIViewController, UITextFieldDelegate {
         let userPassword = passwordTextField.text!
         // Check for empty fields
         if (userEmail.isEmpty) {
-            //Display alert message
-            //displayMyAlertMessage(userMessage: "All fields are required")
-            
-            // Enable this if focus is removed
-            //self.userEmailTextField.resignFirstResponder()
             
             UIView.animate(withDuration: 0.1, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseIn, animations: {self.loginTextField.center.x += 10 }, completion: nil)
             
@@ -38,12 +44,7 @@ class MainController: UIViewController, UITextFieldDelegate {
         }
         
         if (userPassword.isEmpty) {
-            // Display alert message
-            //displayMyAlertMessage(userMessage: "All fields are required")
-            
-            // Enable this if focus is removed
-            //self.userEmailTextField.resignFirstResponder()
-            
+
             UIView.animate(withDuration: 0.1, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseIn, animations: {self.passwordTextField.center.x += 10 }, completion: nil)
             
             UIView.animate(withDuration: 0.1, delay: 0.1, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseIn, animations: {self.passwordTextField.center.x -= 20 }, completion: nil)
@@ -53,8 +54,17 @@ class MainController: UIViewController, UITextFieldDelegate {
             return
         }
         
-        if (wpAuthenticate(loginTextField, passwordTextField)){
-            self.performSegue(withIdentifier: "ShifterPokedexVC", sender: self)
+        //if (wpAuthenticate(loginTextField, passwordTextField)){
+         //   self.performSegue(withIdentifier: "ShifterPokedexVC", sender: self)
+       // }
+        
+        if(login_button.titleLabel?.text == "Logout") {
+            let preferences = UserDefaults.standard
+            preferences.removeObject(forKey: "session")
+            
+            LoginToDo()
+        } else {
+            login_now(username:loginTextField.text!, password: passwordTextField.text!)
         }
         
     }
@@ -65,6 +75,17 @@ class MainController: UIViewController, UITextFieldDelegate {
         //Init routine to hide keyboard
         self.loginTextField.delegate = self
         self.passwordTextField.delegate = self
+        
+        let preferences = UserDefaults.standard
+        if preferences.object(forKey: "session") != nil {
+            login_session   = preferences.object(forKey: "session") as! String
+            check_session()
+        
+        } else {
+            LoginToDo()
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -77,45 +98,166 @@ class MainController: UIViewController, UITextFieldDelegate {
         super.touchesBegan(touches, with: event)
     }
 
-}
 
 
-func wpAuthenticate(_ userTextField: UITextField, _ passTextField: UITextField) -> Bool {
-    let _user = userTextField.text!
-    let _pass = passTextField.text!
     
-  //  var resultValue: String = ""
-    
-    // Store data mysql
-    let myUrl = URL(string: "http://www.702shifters.com/ios_auth.php")
-    var request = URLRequest(url:myUrl!)
-    let postString = ("action=register&user=\(String(describing: _user))&pass=\(_pass)")
-
-    request.httpMethod = "POST";
-    request.httpBody = postString.data(using: String.Encoding.utf8)
-    
-
-    let task = URLSession.shared.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+    func login_now(username:String, password:String) {
+        let post_data: NSDictionary = NSMutableDictionary()
         
-        if error != nil {
-            return
+        
+        post_data.setValue(username, forKey: "username")
+        post_data.setValue(password, forKey: "password")
+        
+        let url:URL = URL(string: self.url)!
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "POST"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        
+        var paramString = ""
+        
+        
+        for (key, value) in post_data {
+            paramString = paramString + (key as! String) + "=" + (value as! String) + "&"
         }
         
-        do {
-            let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
-            if let parseJSON = json {
+        request.httpBody = paramString.data(using: String.Encoding.utf8)
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (
+            data, response, error) in
+            
+            guard let _:Data = data, let _:URLResponse = response  , error == nil else {
                 
-                // Now we can access value
-                let resultValue = parseJSON["Response"] as? String
-                print(resultValue!)
+                return
             }
-        } catch {
-            print(error)
-        }
+            
+            let json: Any?
+            
+            do {
+                json = try JSONSerialization.jsonObject(with: data!, options: [])
+            } catch {
+                return
+            }
+            
+            guard let server_response = json as? NSDictionary else {
+                return
+            }
+            print("login \(server_response)");
+            
+            if let data_block = server_response["data"] as? NSDictionary {
+                if let session_data = data_block["session"] as? String
+                {
+                    self.login_session = session_data
+                    
+                    let preferences = UserDefaults.standard
+                    preferences.set(session_data, forKey: "session")
+                    
+                    DispatchQueue.main.async(execute: self.LoginDone)
+                }
+            }
+            
+        })
+        
+        task.resume()
+        
+        
     }
-    task.resume()
     
-    return true
+    
+    
+    func LoginDone() {
+        //loginTextField.isEnabled = false
+        //passwordTextField.isEnabled = false
+        
+        //login_button.isEnabled = true
+        
+        
+        //login_button.setTitle("Logout", for: .normal)
+        
+        self.performSegue(withIdentifier: "ShifterPokedexVC", sender: self)
+    }
+    
+    func LoginToDo()
+    {
+        loginTextField.isEnabled = true
+        passwordTextField.isEnabled = true
+        
+        login_button.isEnabled = true
+        
+        
+        login_button.setTitle("Login", for: .normal)
+    }
+    
+    
+    func check_session()
+    {
+        let post_data: NSDictionary = NSMutableDictionary()
+        
+        
+        post_data.setValue(login_session, forKey: "session")
+        
+        let url:URL = URL(string: self.url)!
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "POST"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        
+        var paramString = ""
+        
+        
+        for (key, value) in post_data
+        {
+            paramString = paramString + (key as! String) + "=" + (value as! String) + "&"
+        }
+        
+        print(paramString)
+        request.httpBody = paramString.data(using: String.Encoding.utf8)
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (
+            data, response, error) in
+            
+            guard let _:Data = data, let _:URLResponse = response  , error == nil else {
+                
+                return
+            }
+            
+            
+            let json: Any?
+            
+            do {
+                json = try JSONSerialization.jsonObject(with: data!, options: [])
+            } catch {
+                return
+            }
+            
+            guard let server_response = json as? NSDictionary else {
+                return
+            }
+            
+            print("check \(server_response)");
+            
+            if let response_code = server_response["response_code"] as? Int  {
+                if(response_code == 200) {
+                    DispatchQueue.main.async(execute: self.LoginDone)
+                    
+                } else {
+                    DispatchQueue.main.async(execute: self.LoginToDo)
+                }
+            }
+        })
+        
+        task.resume()
+        
+    }
+    
+
 }
+
+
+
 
 
